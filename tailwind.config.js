5bedab895512eefeb59baa4db8f5381be3c7dec0
Igor Martins <iguinhoigor4@gmail.com>
2026-05-24 20:23:07 -0300

    refactor: trocar fonte Inter por DM Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ export default {
         },
       },
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['DM Sans', 'system-ui', 'sans-serif'],
         mono: ['"JetBrains Mono"', 'monospace'],
       },
     },
